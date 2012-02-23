3cb4df4b1d3921e1f7cdb0f81b588938f5668301
Marian Schubert <maio@netsafe.cz>
2012-02-23 15:18:04 +0000

    Emacs settings

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -10,6 +10,7 @@
 
 (defvar my-packages '(starter-kit starter-kit-lisp starter-kit-bindings
                                   paredit project
+                                  goto-last-change
                                   clojure-mode midje-mode
                                   php-mode
                                   yasnippet yasnippet-bundle)
@@ -24,14 +25,23 @@
 (setq ffip-patterns '("*.clj", "*.goap", "*.body", "*.tt", "*.tt2", "*.pm", "*.pl", "*.php"))
 (setq ffip-limit 10000)
 
+(setq auto-mode-alist (cons '("\\.t$" . perl-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.body$" . sql-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.spec$" . sql-mode) auto-mode-alist))
+
+(defun edit-init () (interactive) (find-file "~/.emacs.d/init.el"))
+
+(require 'feature-mode)
+
 ; VIM emulation
 (add-to-list 'load-path "~/.emacs.d/evil")
 (require 'evil)
 (evil-mode 1)
+(setq evil-move-cursor-back nil)
 (define-key evil-insert-state-map "\C-c" 'evil-force-normal-state)
 (define-key evil-normal-state-map ",b" 'switch-to-buffer)
 (define-key evil-normal-state-map ",," 'evil-buffer)
-(define-key evil-insert-state-map ",w" 'evil-write)
+(define-key evil-normal-state-map ",v" 'edit-init)
 (define-key evil-normal-state-map ",w" 'evil-write)
 (define-key evil-normal-state-map ",t" 'find-file-in-project)
 (define-key evil-motion-state-map (kbd "TAB") "%")
