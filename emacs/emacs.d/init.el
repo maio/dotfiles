782224347c722419c58f8eea1db77209fc82c1d6
Marian Schubert <maio@netsafe.cz>
2012-02-28 12:18:23 +0000

    Perl settings

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -26,7 +26,6 @@
 (setq ffip-patterns '("*.clj", "*.goap", "*.body", "*.tt", "*.tt2", "*.pm",
                       "*.pl", "*.php", "*.t", "*.feature"))
 
-(setq auto-mode-alist (cons '("\\.t$" . perl-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("\\.body$" . sql-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("\\.spec$" . sql-mode) auto-mode-alist))
 
@@ -50,3 +49,17 @@
 (add-to-list 'load-path "~/.emacs.d/evil-plugins/surround")
 (require 'surround)
 (global-surround-mode 1)
+
+(require 'sqlplus)
+
+;; Perl
+(setq auto-mode-alist (cons '("\\.t$" . perl-mode) auto-mode-alist))
+(defalias 'perl-mode 'cperl-mode)
+
+(add-hook 'cperl-mode-hook 'n-cperl-mode-hook t)
+(defun n-cperl-mode-hook ()
+  (setq cperl-indent-parens-as-block t
+        cperl-indent-level 4
+        cperl-close-paren-offset -4)
+  (set-face-background 'cperl-array-face "black")
+  (set-face-background 'cperl-hash-face "black"))
