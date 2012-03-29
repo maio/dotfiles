5d5e7b2b7a4c7393045bfc2ffffd4b4f33120a17
Marian Schubert <marian.schubert@gmail.com>
2012-03-30 00:05:47 +0200

    Helm match plugin

--- emacs/emacs.d/maio-helm.el
+++ emacs/emacs.d/maio-helm.el
@@ -2,9 +2,11 @@
       helm-su-or-sudo "sudo"
       helm-allow-skipping-current-buffer nil
       helm-enable-shortcuts t)
+(setq helm-mp-matching-method 'multi2)
 
 (add-to-list 'load-path "~/.emacs.d/helm")
 (require 'helm-config)
+(require 'helm-match-plugin)
 (helm-mode 1)
 (add-to-list 'load-path "~/.emacs.d/projectile")
 (require 'projectile)
