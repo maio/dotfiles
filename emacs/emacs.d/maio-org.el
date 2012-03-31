dfbc98c0efde5c69ea64a0d9fece1fe143eb2f99
Marian Schubert <marian.schubert@gmail.com>
2012-03-31 15:46:40 +0200

    Org

--- emacs/emacs.d/maio-org.el
+++ emacs/emacs.d/maio-org.el
@@ -1,5 +1,6 @@
 (defun maio/unbind-org-mode-comma ()
   (org-defkey org-mode-map [(control ?,)] nil))
 (add-hook 'org-mode-hook 'maio/unbind-org-mode-comma)
+(add-hook 'org-insert-heading-hook 'evil-insert-state)
 
 (provide 'maio-org)
