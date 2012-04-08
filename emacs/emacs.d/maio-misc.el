b54026f84528c898644f123cdc59d14a6127334e
Marian Schubert <marian.schubert@gmail.com>
2012-04-08 21:03:08 +0200

    Update modules

--- emacs/emacs.d/maio-misc.el
+++ emacs/emacs.d/maio-misc.el
@@ -1,6 +1,6 @@
 (add-to-list 'exec-path "/opt/local/bin")
 (add-to-list 'exec-path "~/bin")
-(set-face-attribute 'default nil :height 150)
+(set-face-attribute 'default nil :height 170)
 (setq make-backup-files nil)
 (setq auto-save-default nil)
 (setq backup-inhibited t)
