af83babf36c0add86dda62ae1dd0302ac8186c18
Marian Schubert <maio@netsafe.cz>
2012-04-11 14:16:46 +0000

    Publish (to Gerrit) magit command

--- emacs/emacs.d/maio-git.el
+++ emacs/emacs.d/maio-git.el
@@ -1,4 +1,11 @@
 (setq magit-rewrite-inclusive nil)
 (setq magit-status-buffer-switch-function 'switch-to-buffer)
 
+;; TODO: bind to some key (probably under P?)
+(defun magit-publish ()
+  (interactive)
+  (magit-section-action (item info)
+    ((commit)
+       (magit-run-git "publish" info)))))
+
 (provide 'maio-git)
