b0522358a275b70a98ced4165d7e2f0553634128
Marian Schubert <maio@netsafe.cz>
2012-04-11 14:17:10 +0000

    Enable helm completion in eshell

--- emacs/emacs.d/maio-shell.el
+++ emacs/emacs.d/maio-shell.el
@@ -4,6 +4,10 @@
 (setq comint-prompt-read-only nil)
 (setq eshell-glob-case-insensitive t)
 
+(add-hook 'eshell-mode-hook
+          #'(lambda ()
+              (define-key eshell-mode-map [remap pcomplete] 'helm-esh-pcomplete)))
+
 (setenv "PATH"
         (concat "/opt/local/bin:/opt/perl/bin:/usr/local/bin:"
                 (getenv "PATH")))
