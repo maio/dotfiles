896cf68d37b1e21e235e76299e820bd7e41cc6ed
Marian Schubert <maio@netsafe.cz>
2012-04-17 12:13:26 +0000

    Improve git submit
    when called on commit publish only that commit - otherwise submit everything.

--- emacs/emacs.d/maio-git.el
+++ emacs/emacs.d/maio-git.el
@@ -2,10 +2,11 @@
 (setq magit-status-buffer-switch-function 'switch-to-buffer)
 
 ;; TODO: bind to some key (probably under P?)
-(defun magit-publish ()
+(defun maio-git-submit ()
   (interactive)
-  (magit-section-action (item info)
-    ((commit)
-       (magit-run-git "publish" info))))
+  (when (not (magit-section-action (item info)
+               ((commit)
+                (message "publish"))))
+    (message "submit")))
 
 (provide 'maio-git)
