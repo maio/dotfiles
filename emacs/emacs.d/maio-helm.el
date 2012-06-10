89a851f36848aec3946173acc52ae29a28daf389
Marian Schubert <marian.schubert@gmail.com>
2012-06-10 10:13:33 +0200

    Do not turn on helm mode globally

--- emacs/emacs.d/maio-helm.el
+++ emacs/emacs.d/maio-helm.el
@@ -6,7 +6,7 @@
 (add-to-list 'load-path "~/.emacs.d/helm")
 (require 'helm-config)
 (require 'helm-match-plugin)
-(helm-mode 1)
+(helm-mode 0)
 (add-to-list 'load-path "~/.emacs.d/projectile")
 (require 'helm-projectile)
 (require 'maio-helm-git)
