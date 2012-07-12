50c30b9cb780490efdd0ff1529c1717984ad1e13
Marian Schubert <maio@netsafe.cz>
2012-07-12 09:43:13 +0000

    bind RET and i in term mode

--- emacs/emacs.d/maio-shell.el
+++ emacs/emacs.d/maio-shell.el
@@ -1,4 +1,5 @@
 ;; Shell
+(require 'term)
 (setq eshell-scroll-to-bottom-on-output t)
 (setq ansi-color-for-comint-mode t)
 (setq comint-prompt-read-only nil)
@@ -11,5 +12,16 @@
 (setenv "PATH"
         (concat "/opt/local/bin:/opt/perl/bin:/usr/local/bin:"
                 (getenv "PATH")))
+
+(define-key term-raw-map escreen-prefix-char escreen-map)
+
+(defun maio/term-enter ()
+  (interactive)
+  (call-interactively 'evil-goto-line)
+  (call-interactively 'end-of-line)
+  (evil-emacs-state))
+
+(evil-define-key 'normal term-raw-map (kbd "i") 'maio/term-enter)
+(evil-define-key 'normal term-raw-map (kbd "RET") 'maio/term-enter)
 
 (provide 'maio-shell)
