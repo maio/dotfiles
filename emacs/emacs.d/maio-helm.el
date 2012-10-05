65af600dbcc9f63476860806cb653eda08eaf7f5
Marian Schubert <maio@netsafe.cz>
2012-10-05 09:35:05 +0000

    Show longer buffer names

--- emacs/emacs.d/maio-helm.el
+++ emacs/emacs.d/maio-helm.el
@@ -1,7 +1,8 @@
 (setq helm-su-or-sudo "sudo"
       helm-allow-skipping-current-buffer nil
       helm-M-x-requires-pattern 0
-      helm-ff-transformer-show-only-basename nil)
+      helm-ff-transformer-show-only-basename nil
+      helm-buffer-max-length 40)
 
 (add-to-list 'load-path "~/.emacs.d/helm")
 (require 'helm-config)
