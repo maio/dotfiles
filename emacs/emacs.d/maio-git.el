7ab194abf501bc3514fb1a55589563f413f0ee96
Marian Schubert <maio@netsafe.cz>
2012-10-12 14:21:06 +0000

    Make it possible to submit topic to gerrit

--- emacs/emacs.d/maio-git.el
+++ emacs/emacs.d/maio-git.el
@@ -1,13 +1,26 @@
 (setq magit-rewrite-inclusive nil)
 (setq magit-status-buffer-switch-function 'switch-to-buffer)
 
+(defvar story-history nil)
+
+(defun maio-current-story ()
+  (nth 0 story-history))
+
+(defun maio-read-story-string ()
+  (interactive)
+  (read-string (concat "Story (" (maio-current-story) "): ") "" 'story-history (maio-current-story)))
+
+(defun maio-get-first-word (s)
+  (first (split-string s " ")))
+
 (defun maio-git-submit ()
   (interactive)
-  (when (not (magit-section-action (item info)
-               ((commit)
-                (message "Going to submit current commit")
-                (magit-run-git "publish" info))))
-    (message "Going to submit all pending commits")
-    (magit-run-git "submit")))
+  (let ((story (maio-read-story-string)))
+    (when (not (magit-section-action (item info)
+                 ((commit)
+                  (message "Going to submit current commit")
+                  (magit-run-git "publish" info))))
+      (message "Going to submit all pending commits")
+      (magit-run-git "submit" (maio-get-first-word story)))))
 
 (provide 'maio-git)
