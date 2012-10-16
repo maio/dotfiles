5ecaf1422f92f5637d41f2031d014abf1bd01317
Marian Schubert <maio@netsafe.cz>
2012-10-16 14:57:14 +0000

    Move X11 copy/paste from ui to misc package

--- emacs/emacs.d/maio-misc.el
+++ emacs/emacs.d/maio-misc.el
@@ -45,4 +45,18 @@
 (remove-hook 'prog-mode-hook 'esk-turn-on-hl-line-mode)
 (hl-line-mode nil)
 
+;; enable copy/paste in terminal
+(unless window-system
+  (when (getenv "DISPLAY")
+    (defun xsel-cut-function (text &optional push)
+      (with-temp-buffer
+        (insert text)
+        (call-process-region (point-min) (point-max) "xsel" nil 0 nil "--clipboard" "--input")))
+    (defun xsel-paste-function ()
+      (let ((xsel-output (shell-command-to-string "xsel --clipboard --output")))
+        (unless (string= (car kill-ring) xsel-output)
+          xsel-output)))
+    (setq interprogram-cut-function 'xsel-cut-function)
+    (setq interprogram-paste-function 'xsel-paste-function)))
+
 (provide 'maio-misc)
--- emacs/emacs.d/maio-ui.el
+++ emacs/emacs.d/maio-ui.el
@@ -13,20 +13,6 @@
   (when (and (fboundp 'ns-toggle-fullscreen) window-system)
     (ns-toggle-fullscreen)))
 
-;; enable copy/paste in terminal
-(unless window-system
-  (when (getenv "DISPLAY")
-    (defun xsel-cut-function (text &optional push)
-      (with-temp-buffer
-        (insert text)
-        (call-process-region (point-min) (point-max) "xsel" nil 0 nil "--clipboard" "--input")))
-    (defun xsel-paste-function ()
-      (let ((xsel-output (shell-command-to-string "xsel --clipboard --output")))
-        (unless (string= (car kill-ring) xsel-output)
-          xsel-output)))
-    (setq interprogram-cut-function 'xsel-cut-function)
-    (setq interprogram-paste-function 'xsel-paste-function)))
-
 ;; custom display modes
 (defun regular-mode ()
   (interactive)
