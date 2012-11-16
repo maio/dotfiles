83e0a9da3739dd91d07d1707666a74520f800eeb
Marian Schubert <maio@netsafe.cz>
2012-11-16 14:40:07 +0000

    Create guard notification hooks
    so that one can customize what happens for given notification type

--- emacs/emacs.d/guard.el
+++ emacs/emacs.d/guard.el
@@ -1,10 +1,34 @@
-(defun guard-notify (type title message)
-  (when (string= type "pending")
-    (set-face-background 'modeline "Black"))
-  (when (string= type "success")
-    (set-face-background 'modeline "ForestGreen"))
-  (when (string= type "failed")
-    (set-face-background 'modeline "Firebrick")
-    (message message)))
+(defvar guard-notify-pending-hook nil "Pending notification")
+(defvar guard-notify-success-hook nil "Success notification")
+(defvar guard-notify-failed-hook nil "Failed notification")
+
+(defun guard-notify (type title body)
+  (let ((notify-hook (intern (concat "guard-notify-" type "-hook"))))
+    (run-hook-with-args notify-hook title body)))
+
+;;; notification handling
+
+;; guard-notify-message
+(defun guard-notify-message-body (title body)
+  (message body))
+
+(add-hook 'guard-notify-failed-hook 'guard-notify-message-body)
+
+;; guard-notify-modeline
+(defcustom guard-notify-modeline-pending-color "Black"
+  "Modeline color for pending notification")
+
+(defun guard-notify-modeline-pending (title body)
+  (set-face-background 'modeline guard-notify-modeline-pending-color))
+
+(defun guard-notify-modeline-success (title body)
+  (set-face-background 'modeline "ForestGreen"))
+
+(defun guard-notify-modeline-failed (title body)
+  (set-face-background 'modeline "Firebrick"))
+
+(add-hook 'guard-notify-pending-hook 'guard-notify-modeline-pending)
+(add-hook 'guard-notify-success-hook 'guard-notify-modeline-success)
+(add-hook 'guard-notify-failed-hook 'guard-notify-modeline-failed)
 
 (provide 'guard)
