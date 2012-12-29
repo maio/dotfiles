c5155b50d59a6381b6e306a6141d31815597da0a
Marian Schubert <marian.schubert@gmail.com>
2012-12-29 20:19:05 +0100

    Override key-chord-define so that it doesn't bind key2 key1
    as I don't want g; to be same as ;g because g; might occur in regular
    text

--- emacs/emacs.d/maio-keys.el
+++ emacs/emacs.d/maio-keys.el
@@ -1,5 +1,18 @@
 (require 'magit)
 (require 'key-chord)
+(setq key-chord-one-key-delay 0.2)
+
+;; custom key-chord-define
+(defun key-chord-define (keymap keys command)
+  (if (/= 2 (length keys))
+      (error "Key-chord keys must have two elements"))
+  ;; Exotic chars in a string are >255 but define-key wants 128..255 for those
+  (let ((key1 (logand 255 (aref keys 0)))
+        (key2 (logand 255 (aref keys 1))))
+    (if (eq key1 key2)
+        (define-key keymap (vector 'key-chord key1 key2) command)
+      (define-key keymap (vector 'key-chord key1 key2) command))))
+
 (key-chord-mode 1)
 (key-chord-define-global (kbd ",,") 'other-buffer-or-window)
 (key-chord-define-global (kbd ";w") 'force-save-buffer)
