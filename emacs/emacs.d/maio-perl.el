8e0f7b4271e4e195a19be45fa5c294f1489168ec
Marian Schubert <maio@dhcp-33.netsafe.cz>
2013-01-02 15:35:28 +0100

    Do not use flycheck in Perl for now
    as it has some problems over Tramp

--- emacs/emacs.d/maio-perl.el
+++ emacs/emacs.d/maio-perl.el
@@ -18,22 +18,10 @@
 (add-to-list 'auto-mode-alist '("\\.tmpl$" . html-mode))
 (add-to-list 'auto-mode-alist '("\\.tt$" . html-mode))
 
-;; flycheck
-(require 'flycheck)
-
-;; this has been submitted to flycheck - remove
-(defvar flycheck-checker-perl
-  '(:command
-    ("perl" "-w" "-c" source)
-    :error-patterns (("\\(.*\\) at \\([^ \n]+\\) line \\([0-9]+\\)[,.\n]" 2 3 nil 1))
-    :modes cperl-mode))
-(add-to-list 'flycheck-checkers 'flycheck-checker-perl)
-
 (eval-after-load 'cperl-mode
   '(progn
      (add-hook 'cperl-mode-hook 'esk-prog-mode-hook)
      (add-hook 'cperl-mode-hook 'autopair-on)
-     (add-hook 'cperl-mode-hook 'flycheck-mode)
      (evil-define-key 'normal cperl-mode-map "=" 'perltidy-dwim)
      (evil-define-key 'visual cperl-mode-map "=" 'perltidy-dwim)
      (key-chord-define cperl-mode-map ";;" 'maio/electric-semicolon)
