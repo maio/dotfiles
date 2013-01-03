ee553d53ce263fa5813200567d623cca812dc296
Marian Schubert <marian.schubert@gmail.com>
2013-01-03 09:16:52 +0100

    Make easy to jump into nREPL buffer

--- emacs/emacs.d/maio-clojure.el
+++ emacs/emacs.d/maio-clojure.el
@@ -3,8 +3,18 @@
 
 (add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
 (add-hook 'nrepl-interaction-mode-hook 'ac-nrepl-setup)
+
 (eval-after-load "auto-complete"
   '(add-to-list 'ac-modes 'nrepl-mode))
+
+(defun maio/nrepl ()
+  (interactive)
+  (if (get-process "nrepl-server")
+      (nrepl-switch-to-repl-buffer)
+    (nrepl-jack-in)))
+
+(eval-after-load "clojure-mode"
+  '(evil-define-key 'normal clojure-mode-map (kbd "gs") 'maio/nrepl))
 
 (evil-define-key 'normal nrepl-mode-map (kbd "RET") 'nrepl-return)
 (defadvice nrepl-return (after normal-state () activate) (evil-normal-state))
