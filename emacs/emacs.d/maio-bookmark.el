ac287a6515dc9d374b7e6f49d97f9c743de0f505
Marian Schubert <maio@netsafe.cz>
2013-01-03 10:26:28 +0100

    Local bookmarks

--- emacs/emacs.d/maio-bookmark.el
+++ emacs/emacs.d/maio-bookmark.el
@@ -2,6 +2,11 @@
 
 ;; bmkp-make-function-bookmark
 
+(setq bookmark-save-flag nil)
+
+(when (file-exists-p "~/.emacs.d/bookmarks.local")
+  (bookmark-load "~/.emacs.d/bookmarks.local" nil 'nosave))
+
 (define-key evil-motion-state-map (kbd "gb") 'bookmark-jump)
 
 (provide 'maio-bookmark)
