a5ac9531413b07f9bcdda12b4a76acaa3b97b9c8
Marian Schubert <marian.schubert@gmail.com>
2013-01-18 00:35:15 +0100

    use eval-after-load in maio-clojure.el

--- emacs/emacs.d/maio-clojure.el
+++ emacs/emacs.d/maio-clojure.el
@@ -1,10 +1,4 @@
-(add-hook 'clojure-mode-hook 'eldoc-mode)
-(add-hook 'nrepl-mode-hook 'autopair-on)
-
-(add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
-(add-hook 'nrepl-interaction-mode-hook 'ac-nrepl-setup)
-
-(eval-after-load "auto-complete"
+(eval-after-load 'auto-complete
   '(add-to-list 'ac-modes 'nrepl-mode))
 
 (defun maio/nrepl ()
@@ -13,12 +7,19 @@
       (nrepl-switch-to-repl-buffer)
     (nrepl-jack-in)))
 
-(eval-after-load "clojure-mode"
-  '(evil-define-key 'normal clojure-mode-map (kbd "gs") 'maio/nrepl))
+(eval-after-load 'clojure-mode
+  '(progn
+     (evil-define-key 'normal clojure-mode-map (kbd "gs") 'maio/nrepl)
+     (add-hook 'clojure-mode-hook 'eldoc-mode)))
 
-(evil-define-key 'normal nrepl-interaction-mode-map (kbd "RET") 'nrepl-return)
-(defadvice nrepl-return (after normal-state () activate) (evil-normal-state))
-(evil-define-key 'normal nrepl-mode-map (kbd "(")
-  (lambda () (interactive) (insert "(") (evil-insert-state)))
+(eval-after-load 'nrepl
+  '(progn
+     (add-hook 'nrepl-mode-hook 'autopair-on)
+     (add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
+     (add-hook 'nrepl-interaction-mode-hook 'ac-nrepl-setup)
+     (defadvice nrepl-return (after normal-state () activate) (evil-normal-state))
+     (evil-define-key 'normal nrepl-interaction-mode-map (kbd "RET") 'nrepl-return)
+     (evil-define-key 'normal nrepl-mode-map (kbd "(")
+       (lambda () (interactive) (insert "(") (evil-insert-state)))))
 
 (provide 'maio-clojure)
