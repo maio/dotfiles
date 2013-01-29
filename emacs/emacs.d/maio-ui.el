15054b5298316ea12b41a1c026633a6a859b6711
Marian Schubert <maio@netsafe.cz>
2013-01-29 15:00:07 +0000

    make emacs look nice in Linux

--- emacs/emacs.d/maio-ui.el
+++ emacs/emacs.d/maio-ui.el
@@ -1,7 +1,9 @@
 (require 'maio-util)
 (global-font-lock-mode 0)
 
-(set-frame-font "Source Code Pro Regular")
+(if (system-type-is-gnu)
+    (set-frame-font "Terminus Bold")
+  (set-frame-font "Source Code Pro Regular"))
 
 (defun maio/no-bold-or-underline-please ()
   (mapc
@@ -12,15 +14,14 @@
 ;; custom display modes
 (defun regular-mode ()
   (interactive)
-  (maio/no-bold-or-underline-please)
   (if (system-type-is-gnu)
-    (set-font-size 120)
+    (set-font-size 160)
     (set-font-size 160)))
 
 (defun presentation-mode ()
   (interactive)
   (if (system-type-is-gnu)
-    (set-font-size 140)
+    (set-font-size 170)
     (set-font-size 170)))
 
 (provide 'maio-ui)
