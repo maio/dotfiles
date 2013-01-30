8cb1bda40d92836328bef98527c3489d134e20e6
Marian Schubert <maio@netsafe.cz>
2013-01-30 01:57:54 +0100

    new auto-complete settings

--- emacs/emacs.d/maio-completion.el
+++ emacs/emacs.d/maio-completion.el
@@ -1,19 +1,10 @@
-;; (setq ac-ignore-case nil)
 (require 'auto-complete-config)
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
 (ac-config-default)
+
 (setq ac-ignore-case nil)
 (setq ac-use-menu-map t)
-(add-to-list 'ac-modes 'eshell-mode)
-(add-to-list 'ac-modes 'sql-mode)
-(add-to-list 'ac-modes 'makefile-gmake-mode)
-(add-to-list 'ac-modes 'conf-colon-mode)
-
-(define-key ac-completing-map "\t" 'ac-complete)
-(define-key ac-completing-map [tab] 'ac-complete)
-(define-key ac-completing-map "\C-n" 'ac-next)
-(define-key ac-completing-map "\C-p" 'ac-previous)
-(define-key ac-completing-map "\r" nil)
-(define-key ac-completing-map [return] nil)
+(setq ac-auto-start 5)
+(setq ac-quick-help-delay 1)
 
 (provide 'maio-completion)
