b61ac09864fa4e9ae0461d7d5e8f0e8b08104635
Marian Schubert <marian.schubert@gmail.com>
2013-02-24 23:26:26 +0100

    erc: autojoin #evil-mode

--- emacs/emacs.d/maio-erc.el
+++ emacs/emacs.d/maio-erc.el
@@ -2,6 +2,6 @@
 
 (setq erc-hide-list '("JOIN" "PART" "QUIT"))
 (setq erc-autojoin-channels-alist
-      '(("freenode.net" "#ejuan" "#dolnykubin" "#emacs")))
+      '(("freenode.net" "#ejuan" "#dolnykubin" "#emacs" "#evil-mode")))
 
 (provide 'maio-erc)
