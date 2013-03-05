51b2aa68538c3d827df12b7e7afd08770971a181
Marian Schubert <marian.schubert@gmail.com>
2013-03-05 21:06:13 +0100

    use default dired view

--- emacs/emacs.d/maio-dired.el
+++ emacs/emacs.d/maio-dired.el
@@ -1,11 +1,6 @@
 (require 'dired)
 (require 'dash)
 
-;; Make dired less verbose
-(require 'dired-details)
-(setq-default dired-details-hidden-string "--- ")
-(dired-details-install)
-
 (evil-define-key 'normal dired-mode-map (kbd "K") 'dired-do-delete)
 
 (provide 'maio-dired)
