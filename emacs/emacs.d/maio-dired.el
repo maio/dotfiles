dc4ddef887deb119b64bc3f9740c5489c9e0a8d6
Marian Schubert <marian.schubert@gmail.com>
2013-03-06 10:54:21 +0100

    bind ;w to finish wdired editation

--- emacs/emacs.d/maio-dired.el
+++ emacs/emacs.d/maio-dired.el
@@ -1,6 +1,7 @@
 (require 'dired)
-(require 'dash)
+(require 'wdired)
 
 (evil-define-key 'normal dired-mode-map (kbd "K") 'dired-do-delete)
+(key-chord-define wdired-mode-map (kbd ";w") 'wdired-finish-edit)
 
 (provide 'maio-dired)
