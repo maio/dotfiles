0f44561c80f7a3df5b609efba208e493a28c4a73
Marian Schubert <maio@netsafe.cz>
2013-05-09 08:04:50 +0000

    add yasnippets into ac sources

--- emacs/emacs.d/maio-completion.el
+++ emacs/emacs.d/maio-completion.el
@@ -2,6 +2,12 @@
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
 (ac-config-default)
 
+(setq-default ac-sources
+              '(ac-source-yasnippet
+                ac-source-abbrev
+                ac-source-dictionary
+                ac-source-words-in-same-mode-buffers))
+
 (setq ac-ignore-case nil)
 (setq ac-use-menu-map t)
 (setq ac-auto-start 3)
