ec210100875eacc0b90df65f46e9cafe29ab3378
Marian Schubert <maio@netsafe.cz>
2013-05-13 08:07:58 +0000

    Enable winner more so that I can undo window changes easily

--- emacs/emacs.d/maio-ui.el
+++ emacs/emacs.d/maio-ui.el
@@ -3,6 +3,7 @@
 (global-font-lock-mode 0)
 ;; Enable font-lock for modes where it makes sense
 (add-hook 'diff-mode-hook 'font-lock-mode)
+(winner-mode 1)
 
 (defun maio/set-font (font size)
   (set-frame-font font)
