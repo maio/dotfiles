4d135730524fd88c2ba14bfe7c0d6e67aeab8bc0
Marian Schubert <marian.schubert@gmail.com>
2013-08-30 23:46:20 +0200

    Display Clojure test failures right in the code buffer

--- emacs/emacs.d/maio-clojure.el
+++ emacs/emacs.d/maio-clojure.el
@@ -7,11 +7,30 @@
       (call-interactively 'nrepl-switch-to-repl-buffer)
     (call-interactively 'nrepl)))
 
+(defun maio/delete-nrepl-error-window ()
+  (interactive)
+  (when (get-buffer "*nrepl-error*")
+    (let ((nrepl-error-window (get-buffer-window (get-buffer "*nrepl-error*"))))
+      (when nrepl-error-window
+        (delete-window nrepl-error-window)))))
+
 (eval-after-load 'clojure-mode
   '(progn
+     (require 'midje-mode)
      (require 'clojure-test-mode)
-     (defadvice clojure-test-run-tests (before save activate) (save-buffer))
-     (key-chord-define clojure-test-mode-map ";e" 'clojure-test-run-tests)
+     (defadvice clojure-test-run-tests (before clear-and-save activate)
+       (maio/delete-nrepl-error-window)
+       (midje-clear-comments)
+       (save-buffer))
+     (defadvice clojure-test-highlight-problem (after comment (line event message pp-actual) activate)
+       (save-excursion
+         (goto-char (point-min))
+         (forward-line (1- line))
+         (beginning-of-line)
+         (call-interactively 'open-line)
+         (indent-according-to-mode)
+         (midje-insert-failure-message message)))
+     (key-chord-define clojure-test-mode-map ";w" 'clojure-test-run-tests)
      (evil-define-key 'normal clojure-mode-map "gs" 'maio/nrepl)
      (evil-define-key 'normal clojure-mode-map (kbd "M-.") 'nrepl-jump)
      (add-hook 'clojure-mode-hook 'eldoc-mode)
