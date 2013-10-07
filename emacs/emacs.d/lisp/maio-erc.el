88b23e65d8a7b21b36b94a01d6756a804353579b
Marian Schubert <marian.schubert@gmail.com>
2013-10-07 14:03:28 +0200

    Store ERC notifications in IRC buffer

--- emacs/emacs.d/lisp/maio-erc.el
+++ emacs/emacs.d/lisp/maio-erc.el
@@ -1,8 +1,16 @@
-(eval-after-load 'erc-join
-  '(progn
-     (setq erc-hide-list '("JOIN" "PART" "QUIT"))
-     (setq erc-autojoin-channels-alist
-           '(("freenode.net" "#ejuan" "#dolnykubin" "#emacs" "#evil-mode"
-              "#clojure")))))
+(require 'erc-join)
+
+(defun maio/erc-notify (match-type nickuserhost msg)
+  (with-current-buffer (get-buffer-create "IRC")
+    (save-excursion
+      (insert (format-time-string "%D %H:%M "))
+      (insert msg))))
+
+(add-hook 'erc-text-matched-hook 'maio/erc-notify)
+
+(setq erc-hide-list '("JOIN" "PART" "QUIT"))
+(setq erc-autojoin-channels-alist
+      '(("freenode.net" "#ejuan" "#dolnykubin" "#emacs" "#evil-mode"
+         "#clojure")))
 
 (provide 'maio-erc)
