5e527a686a85213b5f5c6333ea44a2c6493cd19b
Marian Schubert <marian.schubert@gmail.com>
2013-10-24 23:55:30 +0200

    disable font lock mode in web-mode

--- emacs/emacs.d/maio-web.el
+++ emacs/emacs.d/maio-web.el
@@ -7,4 +7,6 @@
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 
+(add-hook 'web-mode-hook 'font-lock-mode)
+
 (provide 'maio-web)
