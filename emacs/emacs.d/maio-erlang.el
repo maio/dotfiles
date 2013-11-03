166334a1768032266fb70c6b16391d7fb862ea8c
Marian Schubert <marian.schubert@gmail.com>
2013-11-03 20:00:19 +0100

    smartparen <> in erlang

--- emacs/emacs.d/maio-erlang.el
+++ emacs/emacs.d/maio-erlang.el
@@ -7,6 +7,8 @@
   '(progn
      (add-hook 'erlang-mode-hook 'flycheck-mode)
      (add-hook 'erlang-mode-hook 'turn-on-smartparens-mode)
-     (add-hook 'erlang-shell-mode-hook 'turn-on-smartparens-mode)))
+     (add-hook 'erlang-shell-mode-hook 'turn-on-smartparens-mode)
+     (sp-with-modes '(erlang-mode)
+       (sp-local-pair "<" ">"))))
 
 (provide 'maio-erlang)
