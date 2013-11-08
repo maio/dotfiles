fc9551cf4e01c3dda084b163903e2af77cc721b3
Marian Schubert <marian.schubert@gooddata.com>
2013-11-08 17:15:20 +0100

    scroll ffs!

--- emacs/emacs.d/maio-misc.el
+++ emacs/emacs.d/maio-misc.el
@@ -30,6 +30,8 @@
 (setq gc-cons-threshold 20000000)
 (setq help-at-pt-display-when-idle t)
 (setq-default compilation-scroll-output t)
+(add-hook 'compilation-start-hook
+          (lambda (process) (setq compilation-scroll-output t)))
 (help-at-pt-set-timer)
 
 (defun maio/indent ()
