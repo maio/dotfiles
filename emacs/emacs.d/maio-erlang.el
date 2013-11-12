19eae7c696140414c1063d6fd769412228a8d72d
Marian Schubert <marian.schubert@gooddata.com>
2013-11-12 17:47:24 +0100

    spaceeees in erlang

--- emacs/emacs.d/maio-erlang.el
+++ emacs/emacs.d/maio-erlang.el
@@ -14,6 +14,11 @@
     (set (make-local-variable 'tab-stop-list)
          (number-sequence my-tab-width 200 my-tab-width))))
 
+(defun my-add-space-after-sexp-insertion (id action _context)
+  (when (eq action 'insert)
+    (insert "  ")
+    (left-char 1)))
+
 (eval-after-load 'erlang
   '(progn
      (add-to-list 'auto-mode-alist '("rebar\\.config\\'" . erlang-mode))
@@ -21,6 +26,9 @@
      (add-hook 'erlang-mode-hook 'idle-highlight-mode)
      (add-hook 'erlang-mode-hook 'maio/erlang-indent-work)
      (add-hook 'erlang-shell-mode-hook 'turn-on-smartparens-mode)
+     (sp-local-pair 'erlang-mode "(" nil :post-handlers '(:add my-add-space-after-sexp-insertion))
+     (sp-local-pair 'erlang-mode "{" nil :post-handlers '(:add my-add-space-after-sexp-insertion))
+     (sp-local-pair 'erlang-mode "[" nil :post-handlers '(:add my-add-space-after-sexp-insertion))
      (sp-with-modes '(erlang-mode)
        (sp-local-pair "<" ">"))))
 
