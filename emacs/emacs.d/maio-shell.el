62a77ce7062127e21e103b080c6098f80e6d6095
Marian Schubert <marian.schubert@gooddata.com>
2013-11-26 13:27:25 +0100

    make easy to serve directory over HTTP

--- emacs/emacs.d/maio-shell.el
+++ emacs/emacs.d/maio-shell.el
@@ -1,6 +1,7 @@
 ;; Shell
 (require 'term)
 (require 'shell)
+(require 'eshell)
 (setq ansi-color-for-comint-mode t)
 (setq comint-prompt-read-only t)
 (setq eshell-cmpl-ignore-case t)
@@ -23,10 +24,17 @@
   (let ((inhibit-read-only t))
     (erase-buffer)))
 
+(defun eshell/serve ()
+  (interactive)
+  (start-process "serve" (format "*serve %s*" default-directory) "python" "-m" "SimpleHTTPServer")
+  (browse-url "http://localhost:8000/")
+  "Server started at http://localhost:8000/")
+
 (evil-define-key 'normal term-raw-map "i" 'maio/term-enter)
 (evil-define-key 'normal term-raw-map [return] 'maio/term-enter)
 
 (add-hook 'shell-mode-hook 'font-lock-mode)
 (add-hook 'term-mode-hook 'font-lock-mode)
+(set-face-attribute 'eshell-prompt nil :foreground "red" :weight 'bold)
 
 (provide 'maio-shell)
