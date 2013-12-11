d995c31f2883e2be5f565b12c54b4e200e286457
Marian Schubert <marian.schubert@gmail.com>
2013-12-11 09:13:34 +0100

    lazy load org mode

--- emacs/emacs.d/maio-org.el
+++ emacs/emacs.d/maio-org.el
@@ -1,12 +1,13 @@
-(require 'org)
-(require 'org-indent)
-
 (setq org-indent-indentation-per-level 1)
 
-(defun maio/unbind-org-mode-comma ()
-  (org-defkey org-mode-map [(control ?,)] nil))
-(add-hook 'org-mode-hook 'maio/unbind-org-mode-comma)
-(add-hook 'org-mode-hook 'org-indent-mode)
-(add-hook 'org-insert-heading-hook 'evil-insert-state)
+(eval-after-load 'org
+  '(progn
+     (require 'org-indent)
+
+     (defun maio/unbind-org-mode-comma ()
+       (org-defkey org-mode-map [(control ?,)] nil))
+     (add-hook 'org-mode-hook 'maio/unbind-org-mode-comma)
+     (add-hook 'org-mode-hook 'org-indent-mode)
+     (add-hook 'org-insert-heading-hook 'evil-insert-state)))
 
 (provide 'maio-org)
