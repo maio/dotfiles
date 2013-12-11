63d84eca0047231f0b825134cdb721925467b1f2
Marian Schubert <marian.schubert@gmail.com>
2013-12-11 09:10:49 +0100

    kill obsolete stuff

--- emacs/emacs.d/maio-shell.el
+++ emacs/emacs.d/maio-shell.el
@@ -1,6 +1,4 @@
 ;; Shell
-(require 'term)
-(require 'shell)
 (require 'eshell)
 (setq ansi-color-for-comint-mode t)
 (setq comint-prompt-read-only t)
@@ -16,11 +14,6 @@
 (defun maio/insert-last-argument ()
   (interactive)
   (insert (first (last eshell-last-arguments))))
-
-(defun maio/term-enter ()
-  (interactive)
-  (goto-char (point-max))
-  (evil-insert-state))
 
 (defun eshell/clear ()
   "04Dec2001 - sailor, to clear the eshell buffer."
@@ -40,15 +33,12 @@
     (let ((max-mini-window-height 0))
       (with-output-to-string (shell-command (s-join " " commands) standard-output)))))
 
-(evil-define-key 'normal term-raw-map "i" 'maio/term-enter)
-(evil-define-key 'normal term-raw-map [return] 'maio/term-enter)
-
 (defun maio/setup-eshell ()
   (define-key eshell-mode-map (kbd "M-.") 'maio/insert-last-argument)
-  (evil-define-key 'normal eshell-mode-map (kbd "RET") 'eshell-send-input))
+  (evil-define-key 'normal eshell-mode-map (kbd "RET") 'eshell-send-input)
+  (evil-define-key 'normal eshell-mode-map "H" 'eshell-bol)
+  (evil-define-key 'normal eshell-mode-map "L" 'move-end-of-line))
 
-(add-hook 'shell-mode-hook 'font-lock-mode)
-(add-hook 'term-mode-hook 'font-lock-mode)
 (set-face-attribute 'eshell-prompt nil :foreground "red" :weight 'bold)
 (add-hook 'eshell-mode-hook 'maio/setup-eshell)
 
