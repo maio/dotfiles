db9865ffb0099316decf10938bd439911d8ca455
Marian Schubert <marian.schubert@gmail.com>
2013-12-11 09:37:54 +0100

    lazy load stuff required by js2 mode

--- emacs/emacs.d/maio-javascript.el
+++ emacs/emacs.d/maio-javascript.el
@@ -1,10 +1,9 @@
-(require 'auto-complete)
-(require 'smartparens)
-
 (add-to-list 'auto-mode-alist '("\\.js" . js2-mode))
 
 (eval-after-load 'js2-mode
   '(progn
+     (require 'auto-complete)
+     (require 'smartparens)
      (add-hook 'js2-mode-hook 'smartparens-mode)
      (add-hook 'js2-mode-hook 'auto-complete-mode)
      (key-chord-define js2-mode-map ";;" 'maio/electric-semicolon)))
