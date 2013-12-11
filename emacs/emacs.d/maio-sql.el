68ba2519bed776d61020d17ead29a869c415285e
Marian Schubert <marian.schubert@gmail.com>
2013-12-11 09:21:13 +0100

    kill sqlplus mode

--- emacs/emacs.d/maio-sql.el
+++ emacs/emacs.d/maio-sql.el
@@ -1,34 +1,7 @@
-(require 'sqlplus)
-(require 'maio-completion)
-
-(add-to-list 'ac-modes 'sqlplus-mode)
 (add-to-list 'ac-modes 'sql-mode)
 (add-to-list 'auto-mode-alist '("\\.sqlt$" . sql-mode))
 (add-to-list 'auto-mode-alist '("\\.body$" . sql-mode))
 (add-to-list 'auto-mode-alist '("\\.spec$" . sql-mode))
-
-;; sqlplus auto complete tables
-(defvar ac-sqlplus-tables-cache nil)
-
-(defun ac-sqlplus-table-candidates ()
-  (or ac-sqlplus-tables-cache
-      (set (make-local-variable 'ac-sqlplus-tables-cache)
-           (mapcar 'downcase (mapcar 'car (cdr (assoc 'table (sqlplus-get-objects-alist))))))))
-
-(ac-define-source sqlplus-tables
-  '((candidates . ac-sqlplus-table-candidates)
-    (symbol . "t")
-    (cache)))
-
-(defun ac-sqlplus-mode-setup ()
-  (setq ac-sources (append '(ac-source-yasnippet ac-source-sqlplus-tables) ac-sources)))
-
-(add-hook 'sqlplus-mode-hook 'ac-sqlplus-mode-setup)
-
-(key-chord-define sqlplus-mode-map ";e"
-                  (lambda () (interactive)
-                    (call-interactively 'sqlplus-send-current)
-                    (evil-normal-state)))
 
 (evil-define-key 'normal sql-mode-map "-" 'maio/find-alternative-file)
 
