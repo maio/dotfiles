ccff7a7cb5915b19abf97425895269e675f48ea5
Marian Schubert <marian.schubert@gmail.com>
2013-12-18 16:56:57 +0100

    load bookmark+ when running helm-bookmarks

--- emacs/emacs.d/maio-bookmark.el
+++ emacs/emacs.d/maio-bookmark.el
@@ -4,6 +4,8 @@
 
 (key-chord-define-global ";j" 'helm-bookmarks)
 
+(defadvice helm-bookmarks (before bmkp () activate) (require 'bookmark+))
+
 (add-hook 'bookmark-after-jump-hook 'recenter-top-bottom)
 
 (provide 'maio-bookmark)
