cb92c204f360657f3b235ed5685c433d63dc4315
Marian Schubert <marian.schubert@gmail.com>
2013-12-29 11:42:41 +0100

    simplify magit notes code

--- emacs/emacs.d/lisp/maio-magit-notes.el
+++ emacs/emacs.d/lisp/maio-magit-notes.el
@@ -1,18 +1,19 @@
-(defun maybe-read-file-to-string (fname)
-  (when (file-exists-p fname) (flymake-read-file-to-string fname)))
+(require 'f)
 
 (defun magit-branch-notes-file ()
-  (s-concat ".git/info/" (magit-get-current-branch) ".org"))
+  (f-join ".git" "info" (concat (magit-get-current-branch) ".org")))
 
 (defun magit-edit-branch-notes-file ()
   (interactive)
   (find-file (magit-branch-notes-file)))
 
 (defun magit-insert-notes ()
-  (let ((notes (maybe-read-file-to-string (magit-branch-notes-file))))
-    (when notes
-      (magit-with-section (section notes 'notes "Notes:" t)
-        (insert notes)
-        (insert "\n")))))
+  (when (file-exists-p (magit-branch-notes-file))
+    (magit-with-section (section notes 'notes "Notes:" t)
+      (insert
+       (with-temp-buffer
+         (insert-file-contents (magit-branch-notes-file))
+         (buffer-string)))
+      (insert "\n"))))
 
 (provide 'maio-magit-notes)
