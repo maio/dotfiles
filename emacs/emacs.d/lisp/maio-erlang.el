5b28f76c82baa264a934aca6acf5242cafb0dd58
Marian Schubert <marian.schubert@gmail.com>
2014-01-05 21:42:02 +0100

    use prog mode hook in erlang mode

--- emacs/emacs.d/lisp/maio-erlang.el
+++ emacs/emacs.d/lisp/maio-erlang.el
@@ -14,10 +14,10 @@
   '(progn
      (add-to-list 'auto-mode-alist '("rebar\\.config\\'" . erlang-mode))
      (add-hook 'erlang-mode-hook 'turn-on-smartparens-mode)
-     (add-hook 'erlang-mode-hook 'idle-highlight-mode)
      (add-hook 'erlang-mode-hook 'maio/setup-tab-indent)
      (add-hook 'erlang-mode-hook 'turn-on-sackspace)
      (add-hook 'erlang-mode-hook 'flycheck-mode)
+     (add-hook 'erlang-mode-hook 'maio/run-prog-mode-hook)
      (add-hook 'erlang-shell-mode-hook 'turn-on-smartparens-mode)
      (define-key erlang-mode-map (kbd "M-r") 'raise-exp)
      (sp-local-pair 'erlang-mode "(" nil :post-handlers '(:add my-add-space-after-sexp-insertion))
