c7c498cd48455eb9984966b60cefc4a755baf356
Marian Schubert <marian.schubert@gmail.com>
2014-01-11 17:03:26 +0100

    eink - make region background lighter

--- emacs/emacs.d/themes/eink-theme.el
+++ emacs/emacs.d/themes/eink-theme.el
@@ -4,7 +4,7 @@
 (custom-theme-set-faces
  'eink
  '(font-lock-builtin-face ((t (:background "white smoke" :foreground "#262626"))))
- '(region ((t (:background "#949494" :foreground "white smoke"))))
+ '(region ((t (:background "grey85" :foreground "white smoke"))))
  '(button ((t (:foreground "#262626" :underline t))))
  '(highlight ((t nil)))
  '(cursor ((t (:background "#949494" :foreground "white smoke"))))
