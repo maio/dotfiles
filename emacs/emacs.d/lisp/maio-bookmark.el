1083753ff1e568baa57abcf707a293753369afe3
Marian Schubert <marian.schubert@gmail.com>
2014-01-20 10:52:07 +0100

    advice C-x r b

--- emacs/emacs.d/lisp/maio-bookmark.el
+++ emacs/emacs.d/lisp/maio-bookmark.el
@@ -1,6 +1,7 @@
 (setq bookmark-save-flag 1)
 
 (defadvice helm-bookmarks (before bmkp () activate) (require 'bookmark+))
+(defadvice bookmark-jump (before bmkp () activate) (require 'bookmark+))
 
 (add-hook 'bookmark-after-jump-hook 'recenter-top-bottom)
 
