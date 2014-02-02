37af202e8900ab243bdc7d894fbf9c676020f5f7
Marian Schubert <marian.schubert@gmail.com>
2014-02-02 16:55:15 +0100

    C-x , or . to switch buffers (useful in terminal)

--- emacs/emacs.d/lisp/maio-keys.el
+++ emacs/emacs.d/lisp/maio-keys.el
@@ -8,6 +8,8 @@
 
 (require 'smartrep)
 ;; https://github.com/shishi/.emacs.d/blob/master/inits/20-smartrep.el
+(smartrep-define-key global-map "C-x" '(("," . previous-buffer)
+                                        ("." . next-buffer)))
 
 (global-set-key (kbd "C-x C-s") 'force-save-buffer)
 (global-set-key (kbd "C-x k") 'kill-current-buffer)
