98a360a66e292071d66411127aab85a442083ffd
Marian Schubert <marian.schubert@gmail.com>
2014-02-03 23:14:49 +0100

    remove stuff I don't use from maio-eshell.el

--- emacs/emacs.d/lisp/maio-eshell.el
+++ emacs/emacs.d/lisp/maio-eshell.el
@@ -12,7 +12,6 @@
      (require 'em-term)
      (require 'em-cmpl)
      (setenv "PAGER" "cat")
-     (set-face-attribute 'eshell-prompt nil :foreground "turquoise1")
      (add-hook 'eshell-mode-hook ;; for some reason this needs to be a hook
                '(lambda () (define-key eshell-mode-map "\C-a" 'eshell-bol)))
      (setq eshell-cmpl-cycle-completions nil)
@@ -26,58 +25,9 @@
                   '("tar" "\\(\\.tar|\\.tgz\\|\\.tar\\.gz\\)\\'"))))
 
 ;;;###autoload
-(defun eshell/cds ()
-  "Change directory to the project's root."
-  (eshell/cd (locate-dominating-file default-directory "src")))
-
-;;;###autoload
-(defun eshell/cds ()
-  "Change directory to the project's root."
-  (eshell/cd (locate-dominating-file default-directory "src")))
-
-;;;###autoload
-(defun eshell/cdl ()
-  "Change directory to the project's root."
-  (eshell/cd (locate-dominating-file default-directory "lib")))
-
-;;;###autoload
 (defun eshell/cdg ()
   "Change directory to the project's root."
   (eshell/cd (locate-dominating-file default-directory ".git")))
-
-;; these two haven't made it upstream yet
-;;;###autoload
-(when (not (functionp 'eshell/find))
-  (defun eshell/find (dir &rest opts)
-    (find-dired dir (mapconcat (lambda (arg)
-                                 (if (get-text-property 0 'escaped arg)
-                                     (concat "\"" arg "\"")
-                                   arg))
-                               opts " "))))
-
-;;;###autoload
-(when (not (functionp 'eshell/rgrep))
-  (defun eshell/rgrep (&rest args)
-    "Use Emacs grep facility instead of calling external grep."
-    (eshell-grep "rgrep" args t)))
-
-;;;###autoload
-(defun eshell/extract (file)
-  (let ((command (some (lambda (x)
-                         (if (string-match-p (car x) file)
-                             (cadr x)))
-                       '((".*\.tar.bz2" "tar xjf")
-                         (".*\.tar.gz" "tar xzf")
-                         (".*\.bz2" "bunzip2")
-                         (".*\.rar" "unrar x")
-                         (".*\.gz" "gunzip")
-                         (".*\.tar" "tar xf")
-                         (".*\.tbz2" "tar xjf")
-                         (".*\.tgz" "tar xzf")
-                         (".*\.zip" "unzip")
-                         (".*\.Z" "uncompress")
-                         (".*" "echo 'Could not extract the file:'")))))
-    (eshell-command-result (concat command " " file))))
 
 (defface esk-eshell-error-prompt-face
   '((((class color) (background dark)) (:foreground "red" :bold t))
@@ -93,40 +43,5 @@
                 (add-text-properties
                  (save-excursion (beginning-of-line) (point)) (point-max)
                  '(face esk-eshell-error-prompt-face))))))
-
-(defun esk-eshell-in-dir (&optional prompt)
-  "Change the directory of an existing eshell to the directory of the file in
-  the current buffer or launch a new eshell if one isn't running.  If the
-  current buffer does not have a file (e.g., a *scratch* buffer) launch or raise
-  eshell, as appropriate.  Given a prefix arg, prompt for the destination
-  directory."
-  (interactive "P")
-  (let* ((name (buffer-file-name))
-         (dir (cond (prompt (read-directory-name "Directory: " nil nil t))
-                    (name (file-name-directory name))
-                    (t nil)))
-         (buffers (delq nil (mapcar (lambda (buf)
-                                      (with-current-buffer buf
-                                        (when (eq 'eshell-mode major-mode)
-                                          (buffer-name))))
-                                    (buffer-list))))
-         (buffer (cond ((eq 1 (length buffers)) (first buffers))
-                       ((< 1 (length buffers)) (ido-completing-read
-                                                "Eshell buffer: " buffers nil t
-                                                nil nil (first buffers)))
-                       (t (eshell)))))
-    (with-current-buffer buffer
-      (when dir
-        (eshell/cd (list dir))
-        (eshell-send-input))
-      (end-of-buffer)
-      (pop-to-buffer buffer))))
-
-;; Port features from
-;; http://blog.peepcode.com/tutorials/2009/shell-method-missing/shell_method_missing.rb
-;; * cloning git repos, github repos
-;; * downloading http urls
-;; * extracting archives
-;; * changing to directories
 
 (provide 'maio-eshell)
