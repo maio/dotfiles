86553fa9074c340a2d4f4cb57ff5b7e15dfc435d
Marian Schubert <marian.schubert@gmail.com>
2014-02-22 11:21:56 +0100

    make maio-erc lazy

--- emacs/emacs.d/lisp/maio-erc.el
+++ emacs/emacs.d/lisp/maio-erc.el
@@ -1,16 +1,17 @@
-(require 'erc-join)
-
 (defun maio/erc-notify (match-type nickuserhost msg)
   (with-current-buffer (get-buffer-create "IRC")
     (save-excursion
       (insert (format-time-string "%D %H:%M "))
       (insert msg))))
 
-(add-hook 'erc-text-matched-hook 'maio/erc-notify)
+(eval-after-load 'erc
+  '(progn
+     (require 'erc-join)
+     (add-hook 'erc-text-matched-hook 'maio/erc-notify)
 
-(setq erc-hide-list '("JOIN" "PART" "QUIT"))
-(setq erc-autojoin-channels-alist
-      '(("freenode.net" "#ejuan" "#dolnykubin" "#emacs" "#evil-mode"
-         "#clojure")))
+     (setq erc-hide-list '("JOIN" "PART" "QUIT"))
+     (setq erc-autojoin-channels-alist
+           '(("freenode.net" "#ejuan" "#dolnykubin" "#emacs" "#evil-mode"
+              "#clojure")))))
 
 (provide 'maio-erc)
