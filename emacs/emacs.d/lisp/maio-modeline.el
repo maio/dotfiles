bc51b353b28c6739ae72945df9530d1fcf9eac72
Marian Schubert <marian.schubert@gmail.com>
2014-02-23 12:14:26 +0100

    diminish cleanup

--- emacs/emacs.d/lisp/maio-modeline.el
+++ emacs/emacs.d/lisp/maio-modeline.el
@@ -1,16 +1,16 @@
 (require 'diminish)
 
-(eval-after-load 'yasnippet '(diminish 'yas-minor-mode "YS"))
 (eval-after-load 'eldoc '(diminish 'eldoc-mode))
 (eval-after-load 'undo-tree '(diminish 'undo-tree-mode))
 (eval-after-load 'hi-lock '(diminish 'hi-lock-mode))
 (eval-after-load 'subword '(diminish 'subword-mode))
-(eval-after-load 'helm '(diminish 'helm-mode))
+(eval-after-load 'simple '(diminish 'auto-fill-function))
+
+(eval-after-load 'yasnippet '(diminish 'yas-minor-mode "YS"))
 (eval-after-load 'company '(diminish 'company-mode "AC"))
 (eval-after-load 'smartparens '(diminish 'smartparens-mode "()"))
-(eval-after-load 'simple '(diminish 'auto-fill-function))
-(eval-after-load 'flymake '(diminish 'flymake-mode "Fly"))
 (eval-after-load 'paredit '(diminish 'paredit-mode "(e)"))
+(eval-after-load 'flymake '(diminish 'flymake-mode "Fly"))
 
 (setq-default
  mode-line-format
