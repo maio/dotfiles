6386d052923a5d31b57ecf7d9f3bfb10782a9ba6
Marian Schubert <marian.schubert@gmail.com>
2014-02-25 22:39:07 +0100

    make easy to pretty print JSON files

--- emacs/emacs.d/lisp/maio-javascript.el
+++ emacs/emacs.d/lisp/maio-javascript.el
@@ -1,9 +1,25 @@
 (add-to-list 'auto-mode-alist '("\\.js" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.json" . json-mode))
 
+(defmacro json-xs-save-point (&rest body)
+  (declare (indent 0) (debug t))
+  `(let ((old-point (point)))
+     ,@body
+     (goto-char old-point)))
+
+(defun json-xs-region (beg end)
+  "Tidy JSON in the region."
+  (interactive "r")
+  (json-xs-save-point
+    (call-process-region beg end "json_xs" t t)))
+
 (eval-after-load 'js2-mode
   '(progn
      (require 'smartparens)
      (add-hook 'js2-mode-hook 'smartparens-mode)))
+
+(eval-after-load 'json-mode
+  '(progn
+     (evil-define-key 'visual json-mode-map "=" 'json-xs-region)))
 
 (provide 'maio-javascript)
