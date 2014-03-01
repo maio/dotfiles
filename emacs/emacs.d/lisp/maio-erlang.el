95093f4215d13e318efa17c0909501ca52797129
Marian Schubert <marian.schubert@gooddata.com>
2014-03-01 17:13:23 +0100

    make easy to jump to erlang shell

--- emacs/emacs.d/lisp/maio-erlang.el
+++ emacs/emacs.d/lisp/maio-erlang.el
@@ -20,6 +20,7 @@
      (add-hook 'erlang-shell-mode-hook 'turn-on-smartparens-mode)
      (define-key erlang-mode-map (kbd "M-r") 'raise-exp)
      (define-key erlang-mode-map (kbd "C-x m t") 'erlang-eunit-compile-and-run-module-tests)
+     (evil-define-key 'normal erlang-mode-map "gs" 'erlang-shell-display)
      (sp-local-pair 'erlang-mode "(" nil :post-handlers '(:add my-add-space-after-sexp-insertion))
      (sp-local-pair 'erlang-mode "{" nil :post-handlers '(:add my-add-space-after-sexp-insertion))
      (sp-local-pair 'erlang-mode "[" nil :post-handlers '(:add my-add-space-after-sexp-insertion))
