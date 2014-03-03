4392dec71eda08afef864b39f38a786a650d800d
Marian Schubert <marian.schubert@gooddata.com>
2014-03-03 22:44:55 +0100

    smartparens in org mode

--- emacs/emacs.d/lisp/maio-org.el
+++ emacs/emacs.d/lisp/maio-org.el
@@ -8,6 +8,7 @@
        (org-defkey org-mode-map [(control ?,)] nil))
      (add-hook 'org-mode-hook 'maio/unbind-org-mode-comma)
      (add-hook 'org-mode-hook 'org-indent-mode)
+     (add-hook 'org-mode-hook 'turn-on-smartparens-mode)
      (add-hook 'org-insert-heading-hook 'evil-insert-state)))
 
 (provide 'maio-org)
