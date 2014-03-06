07feded4d83e6cc2abc6e74ab4a20e32368ee812
Marian Schubert <marian.schubert@gooddata.com>
2014-03-06 22:37:10 +0100

    improve mouse scrolling

--- emacs/emacs.d/lisp/maio-misc.el
+++ emacs/emacs.d/lisp/maio-misc.el
@@ -45,8 +45,6 @@
 
 (put 'narrow-to-region 'disabled nil)
 (setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))
-(setq mouse-wheel-progressive-speed nil)
-(setq mouse-wheel-follow-mouse 't)
 (setq scroll-step           1
       scroll-conservatively 10000)
 
