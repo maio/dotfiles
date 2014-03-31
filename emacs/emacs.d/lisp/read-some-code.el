ba17b82f45fb2fd38a98559b0aa8b3e52bfbfd2d
Marian Schubert <marian.schubert@gooddata.com>
2014-03-31 21:58:20 +0200

    read-some-code.el: use monospaced font (pre tag) for everything

--- emacs/emacs.d/lisp/read-some-code.el
+++ emacs/emacs.d/lisp/read-some-code.el
@@ -7,6 +7,10 @@
 ;; TODO
 ;; - turn off minor modes (flyspell, git gutter, ...)
 
+(defun search-replace-in-buffer (search replace)
+  (beginning-of-buffer)
+  (while (re-search-forward search nil t) (replace-match replace)))
+
 (defun buffer-to-epub (epub-file)
   (interactive "F")
   (when (not (s-ends-with? ".epub" epub-file))
@@ -15,9 +19,10 @@
         (html-file (make-temp-file "buffer-to-epub" nil ".html")))
     (with-current-buffer html-buffer
       ;; htmlize output contains mix of pre and span tags but iBooks
-      ;; uses different fonts for each and resulting document looks
-      ;; weird. div uses same font as span so this should fix it.
-      (while (re-search-forward "</?\\(pre\\)" nil t)
-        (replace-match "div style=\"font-family: monospace; white-space: pre-wrap;\"" nil nil nil 1))
+      ;; uses different fonts for each. It ignores monospace css
+      ;; setting for span tags so we have to replace them with inline
+      ;; pre tags in order to get consistent look.
+      (search-replace-in-buffer "<span" "<pre style=\"display: inline\"")
+      (search-replace-in-buffer "</span" "</pre")
       (write-file html-file))
     (shell-command (format "%s %s %s %s" ebook-convert-bin html-file epub-file ebook-convert-options))))
