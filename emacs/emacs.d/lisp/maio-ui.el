1ba622065a1a634414901f40d851950c4f30a01c
Marian Schubert <marian.schubert@gmail.com>
2014-05-22 15:52:58 +0200

    use slightly bigger font

--- emacs/emacs.d/lisp/maio-ui.el
+++ emacs/emacs.d/lisp/maio-ui.el
@@ -24,7 +24,7 @@
   (if (system-type-is-gnu)
       (maio/set-font "Source Code Pro" 120)
     (progn
-      (maio/set-font "Source Code Pro Medium" 100)
+      (maio/set-font "Source Code Pro Medium" 120)
       (when (ui-type-is-gui)
         (toggle-frame-fullscreen)))))
 
