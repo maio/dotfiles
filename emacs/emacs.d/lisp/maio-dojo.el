f0c32add61c2471e4bfb0f2da85abf2115264be8
Marian Schubert <marian.schubert@gooddata.com>
2014-09-10 18:11:21 +0200

    dojo mode improvements

--- emacs/emacs.d/lisp/maio-dojo.el
+++ emacs/emacs.d/lisp/maio-dojo.el
@@ -3,6 +3,10 @@
   (setq company-idle-delay 0.1)
   (setq shift-select-mode t)
   (setq evil-normal-state-cursor '("red" box))
+  (setq evil-insert-state-cursor '("black" (bar . 2)))
+  (setq evil-emacs-state-cursor '("black" (bar . 2)))
+  (blink-cursor-mode t)
+  (setq truncate-partial-width-windows 40)
   (with-eval-after-load 'parenface
     (let ((color "gray60"))
       (set-face-foreground 'parenface-paren-face color)
