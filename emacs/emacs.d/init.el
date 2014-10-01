866b43810e981b33ec32e9dba485bc4f3e99ce08
Marian Schubert <marian.schubert@gooddata.com>
2014-10-01 15:31:10 +0200

    org mode repo

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -5,6 +5,7 @@
 (require-and-exec 'package
   (add-to-list 'package-archives
                '("melpa" . "http://melpa.milkbox.net/packages/") t)
+  (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
   (package-initialize)
   (when (not package-archive-contents)
     (package-refresh-contents))
