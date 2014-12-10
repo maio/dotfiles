663ca434f27f1c7d9c8885a098494888987f9940
Marian Schubert <marian.schubert@gmail.com>
2014-12-10 21:34:17 +0100

    load LANG env variable from shell

--- emacs/emacs.d/lisp/maio-experiments.el
+++ emacs/emacs.d/lisp/maio-experiments.el
@@ -53,11 +53,13 @@
 (define-key ctl-x-4-map "t" 'toggle-window-split)
 
 ;; read env from shell
-(ensure-package 'exec-path-from-shell)
-(require 'exec-path-from-shell)
-(add-to-list 'exec-path-from-shell-variables "ERL_LIBS")
-(add-to-list 'exec-path-from-shell-variables "PERL5LIB")
-(exec-path-from-shell-initialize)
+(when (ui-type-is-gui)
+  (ensure-package 'exec-path-from-shell)
+  (require 'exec-path-from-shell)
+  (add-to-list 'exec-path-from-shell-variables "ERL_LIBS")
+  (add-to-list 'exec-path-from-shell-variables "PERL5LIB")
+  (add-to-list 'exec-path-from-shell-variables "LANG")
+  (exec-path-from-shell-initialize))
 
 ;; org-capture
 (with-eval-after-load 'org
