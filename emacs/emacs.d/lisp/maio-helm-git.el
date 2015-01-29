4e51d4e1d7bb37b9947559818defec3866333242
Marian Schubert <marian.schubert@gmail.com>
2015-01-29 17:19:40 +0100

    make possible to run helm-cmd-t directly from helm-find-files

--- emacs/emacs.d/lisp/maio-helm-git.el
+++ emacs/emacs.d/lisp/maio-helm-git.el
@@ -5,4 +5,15 @@
 
 (define-key helm-find-files-map (kbd "C-x g") 'helm-ff-run-magit-status)
 
+(defun helm-ff-cmd-t (_candidate)
+  (with-helm-default-directory helm-ff-default-directory
+      (call-interactively 'helm-cmd-t)))
+
+(defun helm-ff-run-cmd-t ()
+  (interactive)
+  (with-helm-alive-p
+    (helm-quit-and-execute-action 'helm-ff-cmd-t)))
+
+(define-key helm-find-files-map (kbd "s-t") 'helm-ff-run-cmd-t)
+
 (provide 'maio-helm-git)
