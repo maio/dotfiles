12e7dac140c6e32c40ea5494d94bdc99b927c6c5
Marian Schubert <marian.schubert@gmail.com>
2015-03-06 20:18:10 +0100

    wip: mc

--- emacs/emacs.d/lisp/maio-multiple-cursors.el
+++ emacs/emacs.d/lisp/maio-multiple-cursors.el
@@ -1,10 +1,21 @@
 (require 'multiple-cursors)
 
 (when evil-mode
-  (defadvice mc/mark-all-like-this-dwim (before emacs-state () activate)
-    (evil-emacs-state))
+  (defun mc/restore-normal-state ()
+    (message "OUT")
+    (evil-force-normal-state)
+    (remove-hook 'multiple-cursors-mode-disabled-hook 'mc/restore-normal-state))
 
-  (add-hook 'multiple-cursors-mode-disabled-hook 'evil-normal-state)
+  (defun mc/insert-state ()
+    (message "IN")
+    ;; (deactivate-mark)
+    (when (evil-normal-state-p)
+      (add-hook 'multiple-cursors-mode-disabled-hook 'mc/restore-normal-state)
+      (evil-insert-state)))
+
+  (add-hook 'multiple-cursors-mode-enabled-hook 'mc/insert-state)
+  (add-hook 'multiple-cursors-mode-disabled-hook 'deactivate-mark)
+
   (define-key mc/keymap [escape] 'mc/keyboard-quit)
   (define-key evil-normal-state-map (kbd "M-;") 'mc/mark-all-like-this-dwim)
   (define-key evil-insert-state-map (kbd "M-;") 'mc/mark-all-like-this-dwim))
