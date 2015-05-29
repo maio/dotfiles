f45972de89e331d06af2553adc3d81a4396cc13b
Marian Schubert <marian.schubert@gmail.com>
2015-05-29 23:10:18 +0200

    indent html by 2

--- emacs/emacs.d/lisp/maio-web.el
+++ emacs/emacs.d/lisp/maio-web.el
@@ -1,3 +1,4 @@
+(setq web-mode-markup-indent-offset 2)
 (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.latte\\'" . web-mode))
