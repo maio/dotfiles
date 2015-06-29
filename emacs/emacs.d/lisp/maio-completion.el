059f66f9fa904cac36426ddda01f39d4710afbc5
Marian Schubert <marian.schubert@gmail.com>
2015-06-29 22:41:51 +0200

    try to resurrect company completion

--- emacs/emacs.d/lisp/maio-completion.el
+++ emacs/emacs.d/lisp/maio-completion.el
@@ -2,10 +2,7 @@
 
 (add-hook 'after-init-hook 'global-company-mode)
 
-(setq company-idle-delay nil)
-(setq company-minimum-prefix-length 3)
-(setq company-dabbrev-downcase nil)
-(push 'company-readline company-backends)
+(setq company-idle-delay 0.1)
 
 (define-key company-active-map [tab] 'company-complete-selection)
 (define-key company-active-map [return] nil)
@@ -13,5 +10,10 @@
 (define-key company-active-map "\C-s" 'company-filter-candidates)
 (define-key company-active-map (kbd "C-n") 'company-select-next)
 (define-key company-active-map (kbd "C-p") 'company-select-previous)
+
+(define-key company-search-map (kbd "RET") 'company-complete-selection)
+(define-key company-search-map [return] 'company-complete-selection)
+(define-key company-search-map (kbd "C-n") 'company-select-next)
+(define-key company-search-map (kbd "C-p") 'company-select-previous)
 
 (provide 'maio-completion)
