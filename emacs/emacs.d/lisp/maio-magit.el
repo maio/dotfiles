147e7412ff45f6e9e39bda53c0ca8e97d4b244f7
Marian Schubert <marian.schubert@gmail.com>
2015-07-01 16:42:08 +0200

    magit cleanup

--- emacs/emacs.d/lisp/maio-magit.el
+++ emacs/emacs.d/lisp/maio-magit.el
@@ -13,10 +13,7 @@
 
 ;; (require 'maio-magit-notes)
 (require 'magit)
-;; (require 'git-rebase-mode)
-;; (require 'git-commit-mode)
 
-;; no need for tags line
 (fullframe magit-status magit-mode-quit-window)
 (fullframe magit-log magit-mode-quit-window)
 
@@ -40,41 +37,5 @@
 
 (defadvice magit-section-toggle (after scroll-line-to-top () activate)
   (recenter 0))
-
-;; (add-hook 'git-commit-mode-hook 'flyspell-mode)
-;; (add-hook 'git-commit-mode-hook 'turn-on-smartparens-mode)
-
-;; (defun magit-blame-kill-commit-id (pos)
-;;   (interactive "d")
-;;   (let* ((chunk (get-text-property pos :blame))
-;;          (commit-info (nth 3 chunk))
-;;          (commit (plist-get commit-info :sha1)))
-;;     (kill-new commit)
-;;     (message commit)))
-
-(defun magit-blame-mark-commit (pos)
-  ;; mark commit - requires modification of magit-refresh-marked-commits-in-buffer
-  ;;
-  ;; equal doesn't work because commit is fully qualified here and
-  ;; log uses abbreviations so it's required to use s-starts-with?
-  (interactive "d")
-  (let* ((chunk (get-text-property pos :blame))
-         (commit-info (nth 3 chunk))
-         (commit (plist-get commit-info :sha1)))
-    (progn
-      (setq magit-marked-commit commit)
-      (magit-refresh-marked-commits))
-    (message (concat "Marked " commit))))
-
-;; (with-eval-after-load 'magit-blame
-;;   (define-key magit-blame-map (kbd "C-w") 'magit-blame-kill-commit-id)
-;;   (define-key magit-blame-map (kbd ".") 'magit-blame-mark-commit)
-
-;;   (defadvice magit-blame-mode (after evil-state () activate)
-;;     (if magit-blame-mode
-;;         (progn
-;;           (evil-emacs-state)
-;;           (recenter))
-;;       (evil-normal-state))))
 
 (provide 'maio-magit)
