2b78e753a1713e223f6743f99815e202a47eba8f
Marian Schubert <marian.schubert@gmail.com>
2015-07-01 16:42:54 +0200

    increase line spacing

--- emacs/emacs.d/lisp/maio-misc.el
+++ emacs/emacs.d/lisp/maio-misc.el
@@ -14,7 +14,7 @@
 (setq yaml-indent-offset 4)
 (setq-default indent-tabs-mode nil)
 (setq-default cursor-in-non-selected-windows nil)
-(setq-default line-spacing 3)
+(setq-default line-spacing 4)
 (column-number-mode 1)
 (idle-highlight-mode 1)
 (setq tramp-default-method "sshx")
