d510470d661d138a36bc2586ffa958298c7e8006
Marian Schubert <marian.schubert@gmail.com>
2015-07-28 09:33:00 +0200

    Fix maio magit reset(?)

--- emacs/emacs.d/lisp/maio-magit.el
+++ emacs/emacs.d/lisp/maio-magit.el
@@ -12,23 +12,16 @@
 (fullframe magit-status magit-mode-quit-window)
 (fullframe magit-log magit-mode-quit-window)
 
-(defun maio-git-reset-hard-tracking ()
+(defun maio-git-reset-master-to-origin ()
   (interactive)
-  (when (yes-or-no-p "Discard all uncommitted changes?")
-    (magit-reset-head-hard
-     (magit-get-tracked-branch (magit-get-current-branch)))))
+  (magit-run-git "fetch" "origin")
+  (magit-run-git "pull" "." "origin/master:master"))
 
 (defun maio-git-reset-hard-origin-master ()
   (interactive)
   (magit-fetch "origin")
   (when (yes-or-no-p "Discard all uncommitted changes?")
-    (magit-reset-head-hard "origin/master")))
-
-(defun maio-git-reset-hard-upstream-master ()
-  (interactive)
-  (magit-fetch "upstream")
-  (when (yes-or-no-p "Discard all uncommitted changes?")
-    (magit-reset-head-hard "upstream/master")))
+    (magit-reset-hard "origin/master")))
 
 (defadvice magit-section-toggle (after scroll-line-to-top () activate)
   (recenter 0))
