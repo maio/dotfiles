46833b2a5f179b3cdd29d69905b357ec02f0672f
Marian Schubert <marian.schubert@gmail.com>
2015-11-12 09:31:20 +0100

    correct css indent

--- emacs/emacs.d/lisp/maio-web.el
+++ emacs/emacs.d/lisp/maio-web.el
@@ -1,6 +1,7 @@
 (setq web-mode-markup-indent-offset 2
       web-mode-script-padding 2
-      web-mode-code-indent-offset 2)
+      web-mode-code-indent-offset 2
+      css-indent-offset 2)
 (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.latte\\'" . web-mode))
