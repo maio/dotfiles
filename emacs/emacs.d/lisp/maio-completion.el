3f218be33ed7ecf17df16fe67c2b27037901827f
Marian Schubert <marian.schubert@gmail.com>
2016-03-06 10:58:55 +0100

    Let's try to use completion once again

--- emacs/emacs.d/lisp/maio-completion.el
+++ emacs/emacs.d/lisp/maio-completion.el
@@ -1,18 +1,22 @@
 (use-package company
-  :defer t
+  :defer 10
   :config
-  (setq company-idle-delay nil)
+  (global-company-mode 1)
+  (setq company-idle-delay 0
+        company-frontends '(company-preview-frontend
+                            company-echo-strip-common-frontend)
+        company-backends '(company-dabbrev-code
+                           company-files
+                           company-css
+                           company-capf))
 
   (define-key company-active-map [tab] 'company-complete-selection)
   (define-key company-active-map [return] nil)
   (define-key company-active-map (kbd "RET") nil)
   (define-key company-active-map "\C-s" 'company-filter-candidates)
-  (define-key company-active-map (kbd "C-n") 'company-select-next)
-  (define-key company-active-map (kbd "C-p") 'company-select-previous)
 
+  (define-key company-search-map [tab] 'company-complete-selection)
   (define-key company-search-map (kbd "RET") 'company-complete-selection)
-  (define-key company-search-map [return] 'company-complete-selection)
-  (define-key company-search-map (kbd "C-n") 'company-select-next)
-  (define-key company-search-map (kbd "C-p") 'company-select-previous))
+  (define-key company-search-map [return] 'company-complete-selection))
 
 (provide 'maio-completion)
