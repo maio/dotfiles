39553d8e101fa399710f182bbf0419c11365f4ab
Marian Schubert <marian.schubert@gmail.com>
2016-03-16 19:46:20 +0100

    tweak company candidates filtering

--- emacs/emacs.d/lisp/maio-completion.el
+++ emacs/emacs.d/lisp/maio-completion.el
@@ -19,6 +19,12 @@
   (define-key company-active-map (kbd "RET") nil)
   (define-key company-active-map "\C-s" 'company-filter-candidates)
 
+  ;; Jump straight to next candidate when starting candidates
+  ;; filtering. Chances are pretty high that it's what I want to
+  ;; complete. If not, I will just filter candidates as usual.
+  (defadvice company-filter-candidates (after next-candidate activate)
+    (company-select-next))
+
   (define-key company-search-map "\C-s" 'company-select-next)
   (define-key company-search-map "\C-r" 'company-select-previous)
   (define-key company-search-map [tab] 'company-complete-selection)
