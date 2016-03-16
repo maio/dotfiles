71e5a6d96e9330416e3f55cc94fbb3e952cb4777
Marian Schubert <marian.schubert@gmail.com>
2016-03-16 19:37:17 +0100

    remove some obsolete magit stuff

--- emacs/emacs.d/lisp/maio-magit.el
+++ emacs/emacs.d/lisp/maio-magit.el
@@ -1,6 +1,5 @@
 (setq magit-status-buffer-switch-function 'switch-to-buffer
       magit-diff-refine-hunk t
-      magit-revert-buffers nil
       magit-repository-directories-depth 2)
 
 (with-eval-after-load 'magit
@@ -15,17 +14,6 @@
 
 (fullframe magit-status magit-mode-quit-window)
 (fullframe magit-log magit-mode-quit-window)
-
-(defun maio-git-reset-master-to-origin ()
-  (interactive)
-  (magit-run-git "fetch" "origin")
-  (magit-run-git "pull" "." "origin/master:master"))
-
-(defun maio-git-reset-hard-origin-master ()
-  (interactive)
-  (magit-fetch "origin")
-  (when (yes-or-no-p "Discard all uncommitted changes?")
-    (magit-reset-hard "origin/master")))
 
 (defadvice magit-section-toggle (after scroll-line-to-top () activate)
   (recenter 0))
