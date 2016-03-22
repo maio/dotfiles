cee5ecd104d86b619f788adab95c3082ea2a92a6
Marian Schubert <marian.schubert@gmail.com>
2016-03-22 09:31:43 +0100

    read-some-code: toc, 1 function per page

--- emacs/emacs.d/lisp/read-some-code.el
+++ emacs/emacs.d/lisp/read-some-code.el
@@ -27,28 +27,39 @@
 
 (defun buffer-to-epub (epub-file)
   (interactive "FOutput file (.epub): ")
-  (let ((epub-file (if (f-ext? epub-file "epub")
-                       epub-file
-                     (concat epub-file ".epub")))
-        (html-buffer (htmlize-buffer (current-buffer)))
-        (author (s-replace "-mode" "" (format "%s" major-mode)))
-        (html-file (make-temp-file "buffer-to-epub" nil ".html")))
-    (with-current-buffer html-buffer
-      ;; htmlize output contains mix of pre and span tags but iBooks
-      ;; uses different fonts for each. It ignores monospace css
-      ;; setting for span tags so we have to replace them with inline
-      ;; pre tags in order to get consistent look.
-      (search-replace-in-buffer "<span" "<pre style=\"display: inline\"")
-      (search-replace-in-buffer "</span" "</pre")
-      (search-replace-in-buffer "<hr />" "<div style=\"page-break-before:always;\"></div>")
-      (write-file html-file))
-    (shell-command
-     (s-join " " (list ebook-convert-bin
-                       (format "\"%s\"" html-file)
-                       (format "\"%s\"" epub-file)
-                       ebook-convert-options
-                       (concat "--authors=" author)
-                       (when ebook-cover
-                         (concat "--cover=" ebook-cover)))))))
+  (let ((source (current-buffer))
+        (source-mode major-mode)
+        (source-name (buffer-name)))
+    (with-temp-buffer
+      (insert-buffer-substring source)
+      (funcall source-mode)
+      (font-lock-fontify-buffer)
+      (defun-page-breaks)
+      (let ((epub-file (if (f-ext? epub-file "epub")
+                           epub-file
+                         (concat epub-file ".epub")))
+            (html-buffer (htmlize-buffer (current-buffer)))
+            (author (s-replace "-mode" "" (format "%s" major-mode)))
+            (html-file (make-temp-file "buffer-to-epub" nil ".html")))
+        (with-current-buffer html-buffer
+          ;; htmlize output contains mix of pre and span tags but iBooks
+          ;; uses different fonts for each. It ignores monospace css
+          ;; setting for span tags so we have to replace them with inline
+          ;; pre tags in order to get consistent look.
+          (search-replace-in-buffer "<span" "<pre style=\"display: inline\"")
+          (search-replace-in-buffer "</span" "</pre")
+          (search-replace-in-buffer "<hr />" "<div style=\"page-break-before:always;\"></div>")
+          (search-replace-in-buffer "^&#12;" "<div style=\"page-break-before:always;\"></div>")
+          (write-file html-file))
+        (shell-command
+         (s-join " " (list ebook-convert-bin
+                           (format "\"%s\"" html-file)
+                           (format "\"%s\"" epub-file)
+                           ebook-convert-options
+                           (concat "--authors=" author)
+                           (concat "--title=" source-name)
+                           "--level1-toc='//*[@class=\"function-name\"]'"
+                           (when ebook-cover
+                             (concat "--cover=" ebook-cover)))))))))
 
 (provide 'read-some-code)
