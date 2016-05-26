ad3b64c2a460ff197f458ace9e24a06b8c68c01f
Marian Schubert <maio@MacBook-Pro.local>
2016-05-26 11:49:48 +0200

    helm git

--- emacs/emacs.d/lisp/maio-helm-git.el
+++ emacs/emacs.d/lisp/maio-helm-git.el
@@ -6,15 +6,13 @@
 (define-key helm-find-files-map (kbd "C-x g") 'helm-ff-run-magit-status)
 (define-key helm-find-files-map (kbd "s-g") 'helm-ff-run-magit-status)
 
-(defun helm-ff-cmd-t (_candidate)
-  (with-helm-default-directory helm-ff-default-directory
-      (call-interactively 'helm-cmd-t)))
-
-(defun helm-ff-run-cmd-t ()
-  (interactive)
-  (with-helm-alive-p
-    (helm-quit-and-execute-action 'helm-ff-cmd-t)))
-
-(define-key helm-find-files-map (kbd "s-t") 'helm-ff-run-cmd-t)
+(use-package helm-ls-git
+  :defer 3
+  :config
+  (global-set-key (kbd "s-t") 'helm-ls-git-ls)
+  (setq helm-ls-git-default-sources '(helm-source-ls-git-buffers
+                                      helm-source-ls-git)
+        helm-ls-git-show-abs-or-relative 'relative)
+  (define-key helm-find-files-map (kbd "s-t") 'helm-ff-run-browse-project))
 
 (provide 'maio-helm-git)
