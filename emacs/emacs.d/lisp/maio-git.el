0fdeadfa367942dbb3303f1857b88904d807eaad
Marian Schubert <marian.schubert@gmail.com>
2016-11-09 06:55:07 +0100

    Remove obsolete diff faces
    Move them to eink theme

--- emacs/emacs.d/lisp/maio-git.el
+++ emacs/emacs.d/lisp/maio-git.el
@@ -1,8 +1,3 @@
-;; remove light background from diff added/removed faces
-(custom-set-faces
- '(diff-added ((t (:inherit diff-changed :foreground "green4"))))
- '(diff-removed ((t (:inherit diff-changed :foreground "red3")))))
-
 (with-eval-after-load 'magit
   (require 'maio-magit))
 
