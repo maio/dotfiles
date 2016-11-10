be1864d9d37cfe5788c50565018cfa257ea581c9
Marian Schubert <marian.schubert@gmail.com>
2016-11-10 08:03:54 +0100

    Try to use dired subtree

--- emacs/emacs.d/lisp/maio-dired.el
+++ emacs/emacs.d/lisp/maio-dired.el
@@ -2,6 +2,12 @@
   (when evil-mode
     (evil-define-key 'normal dired-mode-map "K" 'dired-do-delete)))
 
+(use-package dired-subtree
+  :defer t
+  :config
+  (setq dired-subtree-use-backgrounds nil)
+  (define-key dired-mode-map (kbd "<tab>") 'dired-subtree-toggle))
+
 (add-hook 'dired-mode-hook 'dired-hide-details-mode)
 
 (provide 'maio-dired)
