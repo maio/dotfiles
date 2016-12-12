bd28f341f2cbf6521263da463624620a7f833bb4
Marian Schubert <marian.schubert@gmail.com>
2016-12-12 13:03:35 +0100

    Make TAB/% jump to enclosing tags in web mode

--- emacs/emacs.d/lisp/maio-web.el
+++ emacs/emacs.d/lisp/maio-web.el
@@ -4,6 +4,7 @@
       web-mode-enable-current-element-highlight t
       css-indent-offset 2)
 
+(add-to-list 'auto-mode-alist '("\\.htm\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.latte\\'" . web-mode))
@@ -15,6 +16,7 @@
   (add-hook 'web-mode-hook (lambda () (auto-fill-mode -1)))
   (add-hook 'web-mode-hook 'yas-minor-mode)
   (when evil-mode
+    (evil-define-key 'normal web-mode-map "%" 'web-mode-tag-match)
     (evil-define-key 'normal web-mode-map "3" 'web-mode-comment-or-uncomment)
     (evil-define-key 'visual web-mode-map "3" 'web-mode-comment-or-uncomment)))
 
