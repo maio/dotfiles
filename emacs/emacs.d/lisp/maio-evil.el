af567393545982b83a1744c9f2a274650d9d13e2
Marian Schubert <marian.schubert@gmail.com>
2017-01-23 12:48:37 +0100

    Use correct ESC binding in compilation mode

--- emacs/emacs.d/lisp/maio-evil.el
+++ emacs/emacs.d/lisp/maio-evil.el
@@ -196,7 +196,7 @@
       (evil-add-hjkl-bindings map 'emacs
         "V" 'evil-visual-state)))
 
-  (define-key compilation-mode-map (kbd "ESC") 'evil-normal-state)
+  (define-key compilation-mode-map [escape] 'evil-normal-state)
   (evil-define-key 'normal magit-blame-mode-map "q" 'magit-blame-quit)
   (evil-define-key 'operator magit-blame-mode-map "q" 'magit-blame-quit)
   (evil-define-key 'normal magit-status-mode-map (kbd "TAB") 'magit-section-toggle)
