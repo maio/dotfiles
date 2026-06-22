f6e23b63af36b845cc5d41fc14992dc9a0085a42
Marian Schubert <marian.schubert@gmail.com>
2026-06-22 17:26:48 +0200

    Migrating old doom config

--- emacs/doom/config.el
+++ emacs/doom/config.el
@@ -2,7 +2,8 @@
 
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
-
+;;
+(setq confirm-kill-emacs nil)
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
